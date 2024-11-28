0d796d3c40ff7be471dbba1328abec28152c6189
mthang24242424 <wunkevan@gmail.com>
2024-11-28 23:54:43 +0700

    ss8ex1.c

--- ss8ex1.c
+++ ss8ex1.c
@@ -4,8 +4,8 @@ int main(){
 	int number[3] [3] = {{1,2,3},{4,5,6},{7,8,9}};
 	int sum =0; 
 
-	for(int i=0; i<3; i++){ // in phan tu  
-	for(int j=0; j<3; j++){
+	for(int i=2; i>=0; i--){ // in phan tu  
+	for(int j=2; j>=0; j--){
 		printf(" number[%d][%d] = %d \n",i,j,number[i][j]);
 		} 
 	}
